a0996facdf8bf523dd57e633b18a8efca0b4dcfd
Badr <bsh20351badersh20002@gmail.com>
2021-08-04 11:14:44 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React , {useState} from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+
+
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+
+    setNotes(pre => {
+     return [...pre , newNote];
+
+    })
+
+  }
+
+  function deleteNote(id){
+    setNotes(pre => {
+     return pre.filter((noteItem , index) => {
+//if true deleted then
+        return index !== id
+
+      })
+    })
+
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd = {addNote} />
+    {notes.map((noteItem , index) => {
+      return <Note 
+      key = {index}
+      id ={index}
+        title = {noteItem.title}
+        content = {noteItem.content}
+        onDelete = {deleteNote}
+      />
+        
+      })}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
